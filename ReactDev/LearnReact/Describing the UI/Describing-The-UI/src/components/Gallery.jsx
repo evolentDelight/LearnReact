e9ff4a8cf9b30914083f2dc98067b200a2bbd76d
evolentDelight <evolentDelight@outlook.com>
2025-01-12 13:04:35 -0800

    Components

---
+++ ReactDev/LearnReact/Describing the UI/Describing-The-UI/src/components/Gallery.jsx
@@ -0,0 +1,12 @@
+import Profile from "./Profile"
+
+export default function Gallery(){
+  return(
+    <>
+      <h3>Amazing Scientists (Who I don't know)</h3>
+      <Profile/>
+      <Profile/>
+      <Profile/>
+    </>
+  )
+}
