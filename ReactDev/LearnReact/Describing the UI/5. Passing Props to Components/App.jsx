d2cd0a1a272de79c12e4d252616e00ce5ae66924
evolentDelight <evolentDelight@outlook.com>
2025-02-13 20:21:11 -0800

    5. Passing Props to Components finished

---
+++ ReactDev/LearnReact/Describing the UI/5. Passing Props to Components/App.jsx
@@ -0,0 +1,17 @@
+/* LEARNING OBJECTIVES
+    How to pass props to a component
+    How to read props from a component
+    How to specify default values for props
+    How to pass some JSX to a component
+    How props change over time
+*/
+
+import Profile from "./components/Profile"
+
+export default function App(){
+  return(
+    <>
+      <Profile />
+    </>
+  )
+}
