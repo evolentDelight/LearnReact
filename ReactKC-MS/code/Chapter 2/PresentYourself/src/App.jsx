f4f024b71377331f55a3c971fef45602315e628e
evolentDelight <evolentDelight@outlook.com>
2025-03-18 11:47:03 -0700

    About Me complete

--- ReactKC-MS/code/Chapter 2/PresentYourself/src/App.jsx
+++ ReactKC-MS/code/Chapter 2/PresentYourself/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import myProfilePic from "./assets/my photo.jpg"
 
 function App() {
 
   return (
     <>
-
+      <img src={myProfilePic} id="myPic"/>
+      <h1 id="name">Ben Kwon</h1>
+      <h1 id="title">Intermediate JavaScript Developer, Lifelong Student</h1>
+      <div id="description">
+        <p>I live in California</p>
+        <p>I am an aspiring JavaScript Developer</p>
+      </div>
     </>
   )
 }
