3d412bca47da1e5c140d7cd946f0e2d2617344dc
evolentDelight <evolentDelight@outlook.com>
2025-03-18 13:39:15 -0700

    Goal List Code Complete

--- ReactKC-MS/code/Chapter 2/ReactGoals/src/App.jsx
+++ ReactKC-MS/code/Chapter 2/ReactGoals/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import GoalList from "./components/GoalList.jsx"
+import Header from "./components/Header.jsx"
 
 function App() {
   return (
     <>
-
+      <Header />
+      <GoalList />
     </>
   )
 }
---
+++ ReactKC-MS/code/Chapter 2/ReactGoals/src/components/SecondGoal.jsx
@@ -0,0 +1,8 @@
+export default function SecondGoal(){
+  return(
+    <li>
+      <h2>Achieve mastery in React And React's JavaScript</h2>
+    </li>
+    
+  )
+}
