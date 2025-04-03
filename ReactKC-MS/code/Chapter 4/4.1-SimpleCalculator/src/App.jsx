eb3bcf8d1b2169d12ff1fb928863a9ad5b58a7d1
evolentDelight <evolentDelight@outlook.com>
2025-04-02 21:34:15 -0700

    Added Math Operation Components

--- ReactKC-MS/code/Chapter 4/4.1-SimpleCalculator/src/App.jsx
+++ ReactKC-MS/code/Chapter 4/4.1-SimpleCalculator/src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css"
-import Addition from "./components/Addition"
+import Addition from "./components/Addition.jsx"
+import Subtraction from "./components/Subtraction.jsx"
+import Multiplication from "./components/Multiplication.jsx"
+import Division from "./components/Division.jsx"
 
 function App() {
   return (
     <div className="container">
       <Addition/>
+      <Subtraction/>
+      <Multiplication/>
+      <Division/>
     </div>
   )
 }
