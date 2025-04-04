aefcd61d6f9c88894f1e67ff3603e7ec94c512e3
evolentDelight <evolentDelight@outlook.com>
2025-04-03 19:34:39 -0700

    Add initial CSS styling

--- ReactKC-MS/code/Chapter 4/4.2-EnhancedCalculator/src/App.jsx
+++ ReactKC-MS/code/Chapter 4/4.2-EnhancedCalculator/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Operation from './components/Operation';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [values, setValues] = useState({left: "", right: ""})
 
   return (
-    <>
-    </>
+    <div className='container'>
+      <Operation values={values} setValues={setValues}/>
+    </div>
   )
 }
 
