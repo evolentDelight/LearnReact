c3aecb64056a851c87e4564c9837a07f021dcbf7
evolentDelight <evolentDelight@outlook.com>
2025-04-08 19:17:28 -0700

    Error Message Complete

--- ReactKC-MS/code/Chapter 5/5.1-EmailConditionalErrorMessage/src/App.jsx
+++ ReactKC-MS/code/Chapter 5/5.1-EmailConditionalErrorMessage/src/App.jsx
@@ -1,10 +1,35 @@
-import { useState } from 'react'
+import { useState } from "react"
 import './App.css'
 
 function App() {
+  const [email, setEmail] = useState("")
+  const [error, setError] = useState(false)
+  
+  function handleEmailChange(event){
+    setEmail(event.target.value);
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+
+    setError(!email.includes("@"))//Check if it contains an '@' sign
+  }
 
   return (
     <>
+      <div className="input-container">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="id-email">Your email: </label>
+          <input
+            type="text"
+            id="id-email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        {error && <p className="error-msg">Invalid Email Address Entered!</p>}
+      </div>
     </>
   )
 }
