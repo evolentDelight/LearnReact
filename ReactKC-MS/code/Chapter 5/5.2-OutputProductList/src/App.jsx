a7d5c5151b46f46050f040c440379a89288f086e
evolentDelight <evolentDelight@outlook.com>
2025-04-08 20:34:19 -0700

    Output a list or Products Complete

--- ReactKC-MS/code/Chapter 5/5.2-OutputProductList/src/App.jsx
+++ ReactKC-MS/code/Chapter 5/5.2-OutputProductList/src/App.jsx
@@ -1,12 +1,49 @@
 import { useState } from 'react'
 import './App.css'
 
+let nextId = 3;
+
+const initialProducts = [
+  {
+    id: 0,
+    title: "Product0",
+    price: "$12.99"
+  },
+  {
+    id: 1,
+    title: "Product1",
+    price: "$35.99"
+  },
+  {
+    id: 2,
+    title: "Product2",
+    price: "$50.00"
+  }
+]
 function App() {
-  
+  const [products, setProducts] = useState(initialProducts)
+
+  const handleAddProduct = () =>{
+    setProducts(
+      [
+        ...products,
+        {
+          id: nextId,
+          title: "Another New Product",
+          price: "$200.00"
+        }
+      ]
+    )
+    nextId++;
+  }
 
   return (
-    <>
-    </>
+    <div className="list-container">
+      <button onClick={handleAddProduct}>Add Product</button>
+      {products.map(item =>
+        <p id={item.id}>{item.title} ({item.price})</p>
+      )}
+    </div>
   )
 }
 
