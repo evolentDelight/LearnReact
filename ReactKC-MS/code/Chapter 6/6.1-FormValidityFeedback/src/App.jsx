d70802ea7e8771203aab88e1f5b91f2e36776b98
evolentDelight <evolentDelight@outlook.com>
2025-05-02 16:20:16 -0700

    Validation Logic and CSS complete

--- ReactKC-MS/code/Chapter 6/6.1-FormValidityFeedback/src/App.jsx
+++ ReactKC-MS/code/Chapter 6/6.1-FormValidityFeedback/src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 
+function validateEmail(email) {
+  return /^\S+@\S+.\S/.test(email);
+}
+
+function validatePassword(password) {
+  return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password);
+}
+
 //Form Submission and Validation of user's Email and Password Input
 function App() {
   const [userInput, setUserInput] = useState({
@@ -14,19 +22,32 @@ function App() {
     isPasswordValid: true,
   });
 
-  const handleUserInputChange = (inputType) => () => {
+  const handleUserInputChange = (inputType) => (event) => {
     setUserInput((prevState) => ({
       ...prevState,
-      [inputType]: prevState[inputType],
+      [inputType]: event.target.value,
     }));
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const { email, password } = userInput;
+
+    setValidations({
+      isEmailValid: validateEmail(email),
+      isPasswordValid: validatePassword(password),
+    });
   };
 
   return (
     <>
       <Form
         userInputs={userInput}
+        validations={validations}
         handleEmailInput={handleUserInputChange("email")}
         handlePasswordInput={handleUserInputChange("password")}
+        handleFormSubmit={handleFormSubmit}
       />
     </>
   );
--- ReactKC-MS/code/Chapter 6/6.1-FormValidityFeedback/src/components/Form.jsx
+++ ReactKC-MS/code/Chapter 6/6.1-FormValidityFeedback/src/components/Form.jsx
@@ -1,22 +1,38 @@
 import classes from "./Form.module.css";
 
-export default function Form({}) {
-  let emailInputClass, passwordInputClass;
+export default function Form({
+  userInputs,
+  validations,
+  handleEmailInput,
+  handlePasswordInput,
+  handleFormSubmit,
+}) {
+  let emailInputClass = validations.isEmailValid
+    ? classes.inputValid
+    : classes.inputInvalid;
+  let passwordInputClass = validations.isPasswordValid
+    ? classes.inputValid
+    : classes.inputInvalid;
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleFormSubmit}>
       <label htmlFor="emailInput">Your Email:</label>
       <input
-        className={`${classes.input}, ${emailInputClass}`}
-        type="email"
-        id="emailInput"
+        className={`${classes.input} ${emailInputClass}`}
+        type="text"
+        name="email"
+        value={userInputs.email}
+        onChange={handleEmailInput}
       />
       <label htmlFor="passwordInput">Your Password:</label>
       <input
-        className={`${classes.input}, ${passwordInputClass}`}
+        className={`${classes.input} ${passwordInputClass}`}
         type="password"
-        id="passwordInput"
+        name="password"
+        value={userInputs.password}
+        onChange={handlePasswordInput}
       />
-      <input className={classes.submitButton} type="button" value="Submit" />
+      <input className={classes.submitButton} type="submit" value="Submit" />
     </form>
   );
 }
