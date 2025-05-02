a0ae1027e8cd58e5e4bdd6bd460a734f982f2f43
evolentDelight <evolentDelight@outlook.com>
2025-05-02 12:00:53 -0700

    initial look

--- ReactKC-MS/code/Chapter 6/6.1-FormValidityFeedback/src/components/Form.jsx
+++ ReactKC-MS/code/Chapter 6/6.1-FormValidityFeedback/src/components/Form.jsx
@@ -1,12 +1,23 @@
 import classes from "./Form.module.css";
 
-export default function Form() {
+export default function Form({ handleInput, isValidEmail, isValidPassword }) {
+  let emailInputClass = classes.general;
+  let passwordInputClass = classes.general;
   return (
-    <form>
+    <form className={classes.form}>
       <label htmlFor="emailInput">Your Email:</label>
-      <input type="email" id="emailInput" />
-      <label htmlFor="passwordInput">Your Password</label>
-      <input type="password" id="passwordInput" />
+      <input
+        className={`${classes.input}, ${emailInputClass}`}
+        type="email"
+        id="emailInput"
+      />
+      <label htmlFor="passwordInput">Your Password:</label>
+      <input
+        className={`${classes.input}, ${passwordInputClass}`}
+        type="password"
+        id="passwordInput"
+      />
+      <input className={classes.submitButton} type="button" value="Submit" />
     </form>
   );
 }
