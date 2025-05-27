e0793658a20794bc89ec8e08bda3d165b8a6c172
evolentDelight <evolentDelight@outlook.com>
2025-05-27 11:09:47 -0700

    Experimenting counterRef

--- ReactKC-MS/code/Chapter 7/0. TestCode/src/App.jsx
+++ ReactKC-MS/code/Chapter 7/0. TestCode/src/App.jsx
@@ -2,17 +2,30 @@ import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const inputRef = useRef("");
+  const inputRef = useRef(""); //used for consoleOutputButton
+  const counterRef = useRef(0); //used for counterRef
+  const [triggerRender, setTriggerRender] = useState(0); //used for counterRef
 
   const consoleOutputButton = () => {
     console.log("current.value: ", inputRef.current.value);
     console.log("current: ", inputRef.current);
   };
 
+  const handleCounter = () => {
+    counterRef.current += 1;
+    setTriggerRender((prevState) => prevState + 1);
+  };
+
   return (
     <>
+      <p>For Console Output</p>
       <input type="text" ref={inputRef} />
       <button onClick={consoleOutputButton}>Console Output</button>
+      <hr />
+      <p>For counterRef</p>
+      <p>counterRef: {counterRef.current}</p>
+      <p>triggerRender: {triggerRender}</p>
+      <button onClick={handleCounter}>+1</button>
     </>
   );
 }
