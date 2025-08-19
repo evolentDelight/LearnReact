aa1ccd46572910167d4087a39b01b40600d964d4
evolentDelight <evolentDelight@outlook.com>
2025-08-18 21:13:07 -0700

    Completed exercise

--- ReactKC-MS/code/Chapter 7/7.1-Extracted-User-Input-Values/src/components/Form.jsx
+++ ReactKC-MS/code/Chapter 7/7.1-Extracted-User-Input-Values/src/components/Form.jsx
@@ -1,17 +1,30 @@
+import { useRef } from "react";
+
+const PROGRAMS = {
+  basics: "The Basics",
+  advanced: "Advanced Concepts",
+  mastery: "Mastery",
+};
+
 function Form() {
+  const userName = useRef();
+  const userProgram = useRef();
+
   function formSubmitHandler(event) {
     event.preventDefault();
+    console.log(`User Name: ${userName.current.value}`);
+    console.log(`Selected Program: ${PROGRAMS[userProgram.current.value]}`);
   }
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" />
+        <input ref={userName} type="text" id="name" />
       </div>
       <div className="form-control">
         <label htmlFor="program">Choose your program</label>
-        <select id="program">
+        <select id="program" ref={userProgram}>
           <option value="basics">The Basics</option>
           <option value="advanced">Advanced Concepts</option>
           <option value="mastery">Mastery</option>
