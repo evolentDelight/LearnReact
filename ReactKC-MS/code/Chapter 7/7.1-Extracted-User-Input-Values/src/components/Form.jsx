202b141088757d9c668f2dc422151ac88480d1d9
evolentDelight <evolentDelight@outlook.com>
2025-08-18 21:17:45 -0700

    Edit to better useRef names

--- ReactKC-MS/code/Chapter 7/7.1-Extracted-User-Input-Values/src/components/Form.jsx
+++ ReactKC-MS/code/Chapter 7/7.1-Extracted-User-Input-Values/src/components/Form.jsx
@@ -7,24 +7,24 @@ const PROGRAMS = {
 };
 
 function Form() {
-  const userName = useRef();
-  const userProgram = useRef();
+  const nameRef = useRef();
+  const programRef = useRef();
 
   function formSubmitHandler(event) {
     event.preventDefault();
-    console.log(`User Name: ${userName.current.value}`);
-    console.log(`Selected Program: ${PROGRAMS[userProgram.current.value]}`);
+    console.log(`User Name: ${nameRef.current.value}`);
+    console.log(`Selected Program: ${PROGRAMS[programRef.current.value]}`);
   }
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your name</label>
-        <input ref={userName} type="text" id="name" />
+        <input ref={nameRef} type="text" id="name" />
       </div>
       <div className="form-control">
         <label htmlFor="program">Choose your program</label>
-        <select id="program" ref={userProgram}>
+        <select id="program" ref={programRef}>
           <option value="basics">The Basics</option>
           <option value="advanced">Advanced Concepts</option>
           <option value="mastery">Mastery</option>
